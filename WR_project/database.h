b4fe4be973989699a9d4ed589f57395bfaf582d4
robert <gorkiewicz.r@gmail.com>
2017-03-27 13:32:11 +0200

    have started database.h file
    added private variables

--- WR_project/database.h
+++ WR_project/database.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <fstream>
+#include <string>
 class database
 {
+
+private:
+	
+	//personal info
+	int :: _indexNumber;
+	string :: _name;
+	string :: _surname;
+	string :: _emailAddress;
+
+	//
+	
+
+
+
 public:
 	database();
+
+	void addMember()
 
 
 
