0914bf2d98712c386b0f791ca5e830da844139d4
Wojtek <wojtek.galant97@gmail.com>
2017-03-27 12:59:44 +0200

    Add empty project

---
+++ WR_project/WR_project.cpp
@@ -0,0 +1,16 @@
+// WR_project.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+	_getch();
+    return 0;
+}
+
