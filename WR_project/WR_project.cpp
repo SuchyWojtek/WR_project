3cf7301b6f04408378a27996c3578f9f1a01d623
Wojtek <wojtek.galant97@gmail.com>
2017-03-30 12:51:03 +0200

    Add 1st menu (login)

--- WR_project/WR_project.cpp
+++ WR_project/WR_project.cpp
@@ -5,11 +5,76 @@
 #include <iostream>
 #include <conio.h>
 #include "Login.h"
+#include "contacts.h"
+#include "md5.h"
+#include "string"
 
 using namespace std;
 
+void chooseOperation(int operation)
+{
+	Login l;
+	string login;
+	string password = "";
+	string confirmPassword = "";
+	char c;
+
+	switch (operation)
+	{
+	case 1:
+	{
+		cout << "Login: ";
+		cin >> login;
+		cout << "Password: ";
+		while (c = _getch() != 13)
+		{
+			cout << "*";
+			password += c;
+		}
+		cout << "Confirm Password: ";
+		while (c = _getch() != 13)
+		{
+			cout << "*";
+			confirmPassword += c;
+		}
+		if (password == confirmPassword)
+		{
+			l.newAccount(login,password);
+		}
+		break;
+	}
+	case 2:
+	{
+		cout << "Login: ";
+		cin >> login;
+		cout << "Password: ";
+		while (c = _getch() != 13)
+		{
+			cout << "*";
+			password += c;
+		}
+		l.verifyLogin(login, password);
+		break;
+	}
+	default: {}
+	}
+}
+
+void menu()
+{
+	int operation;
+	string menu = "[1] sign up\n[2] sign in\n";
+
+	cout << menu << "Choose operation: ";
+	cin >> operation;
+
+	chooseOperation(operation);
+
+}
+
 int main()
 {
+	
 	
 	_getch();
     return 0;
